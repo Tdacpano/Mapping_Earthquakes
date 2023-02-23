c363d914d8dd1b208f36f6e31e7a9e9f54244ce5
Taylor Dacpano <tdacpano@ucsd.edu>
2023-02-23 09:46:06 -0800

    Adding GeoJSON Points with Markers

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -35,7 +35,7 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Tdacpano/Mapping_Earthquakes/blob/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/Tdacpano/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
